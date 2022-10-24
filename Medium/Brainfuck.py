2205e2242e796ffcf48c4614ae0ba1f2b8347737
Amheht <63897711+Amheht@users.noreply.github.com>
2022-10-24 02:15:24 -0500

    Update Brainfuck.py
    Added an enum to define errors.
    Removed old error reporting.
    Added error system to log any errors, and then display the error with the highest priority when terminating the program.

--- Medium/Brainfuck.py
+++ Medium/Brainfuck.py
@@ -9,6 +9,7 @@ class CMD_TYPE(Enum):
 
     @classmethod
     def get(cls, val):
+
         if val in ['<','>']:
             return cls.POINTER
         elif val in ['+','-']:
@@ -19,6 +20,25 @@ class CMD_TYPE(Enum):
             return cls.OUTPUT
         elif val == ',':
             return cls.INPUT     
+
+class ERR_TYPE(Enum):
+    VALUE = 1
+    POINTER = 2
+    SYNTAX = 3
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __str__(self):
+        if self.value == 1:
+            return "INCORRECT VALUE"
+        elif self.value == 2:
+            return "POINTER OUT OF BOUNDS"
+        else: 
+            return "SYNTAX ERROR"
 
 c = None
 l, s, n = [int(i) for i in input().split()]
@@ -38,6 +58,7 @@ class PyFuck:
     ''' 
         PyFuck: A Python Implementation of Brainfuck. 
         Written by: Amheht
+
     '''
     def __init__(self, s, program, inputs):
         self.registers =  [0] * s
@@ -53,11 +74,13 @@ class PyFuck:
         self.code = ''
         self.idx = 0
 
+        self.error_log = []
         self._sanitize_code()
         self._bracket_mapping()
 
     def ptr_operation(self, cmd) -> None:
-        ''' Increments or decrements the ptr value by 1. '''     
+        ''' Increments or decrements the ptr value by 1. '''
+        
         if cmd == '<':
             self.ptr += -1
         else: self.ptr += 1
@@ -68,7 +91,7 @@ class PyFuck:
         ''' Checks if the program's pointer is a valid value. ''' 
         if self.ptr < 0 or self.ptr > self.num_registers - 1:
             self.error = True
-            print("POINTER OUT OF BOUNDS")
+            self.error_log.append(ERR_TYPE.POINTER)
 
     def register_operation(self, cmd) -> None:
         ''' Increments or decrements the register indexed at the pointer by 1. '''
@@ -78,7 +101,7 @@ class PyFuck:
 
         if self.registers[self.ptr] < 0 or self.registers[self.ptr] > 255:
             self.error = True
-            print("INCORRECT VALUE")
+            self.error_log.append(ERR_TYPE.VALUE)
 
     def input_operation(self):
         ''' Writes input to pointed register. '''
@@ -104,18 +127,19 @@ class PyFuck:
             elif self.code[idx] == ']':
                 if starts == []:
                     self.error = True
-                    print("SYNTAX ERROR")
-                    break
+                    self.error_log.append(ERR_TYPE.SYNTAX)
+                    return
                 self.loops.append((starts.pop(), idx))
+
+        if len(starts) != 0:
+            self.error = True
+            self.error_log.append(ERR_TYPE.SYNTAX)
 
     def _find_end(self, val) -> int:
         ''' Returns the bracket loop from the given start index. '''
         for loop in self.loops:
             if val == loop[0]:
                 return loop[1]
-
-        self.error = True
-        print("SYNTAX ERROR")
         return -1
 
     def _find_begin(self, val) -> int:
@@ -123,9 +147,6 @@ class PyFuck:
         for loop in self.loops:
             if val == loop[1]:
                 return loop[0]
-    
-        print("SYNTAX ERROR")
-        self.error = True
         return -1
 
     def bracket_operation(self):
@@ -142,7 +163,6 @@ class PyFuck:
         ''' Executed the loaded Brainfuck program. '''
         while self.idx < len(self.code):
             cmd = self.code[self.idx]
-
             cmd_type = CMD_TYPE.get(cmd)
             
             if cmd_type == CMD_TYPE.POINTER:
@@ -155,12 +175,20 @@ class PyFuck:
                 self.output_operation()
             elif cmd_type == CMD_TYPE.INPUT:
                 self.input_operation()
+            else:
+                self.error = True
+
             if self.error:
                 break
+
             self.idx += 1
-          
+            
+
         if not self.error:
             print(self.output)
+        else:
+            err = max(self.error_log)
+            print(err)
 
 main = PyFuck(s, program, inputs)
 main.run()
