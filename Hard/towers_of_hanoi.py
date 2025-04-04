27907ca2dc4ae5248dda89f32b5c62b12de73743
Amheht <63897711+Amheht@users.noreply.github.com>
2025-04-04 16:42:14 -0500

    Update towers_of_hanoi.py
    Cleaned up code.

--- Hard/towers_of_hanoi.py
+++ Hard/towers_of_hanoi.py
@@ -1,14 +1,9 @@
 import sys
-import math
-
-disk_count = int(input())
-turn_count = int(input())
-
 
 class TowersOfHanoi:
-''' A class that generates the state of a Towers of Hanoi game for a given disk and turn count. '''
+    """ A class that generates the state of a Towers of Hanoi game for a given disk and turn count. """
 
-    def __init__(self, num_disks, num_turns = 0):
+    def __init__(self, num_disks: int, num_turns: int = 0):
         self.num_disks = num_disks
         self.L = []
         self.M = []
@@ -16,15 +11,13 @@ class TowersOfHanoi:
         self.direction = [-1,1][num_disks % 2 == 0]
         self.render_disk_max = (2 * num_disks) + 1
         self.state = ""
-        self.turns = num_turns
-        if num_turns:
-          self.generate_state(turn_count)
+        self.num_turns = num_turns
+        self.generate_state()
 
-    def generate_state(self, turn_count):
-      ''' Generates a Towers of Hanoi game state based on the turn_count parameter.'''
-        self.turns = turn_count
+    def generate_state(self) -> None:
+        """ Generates a Towers of Hanoi game state based on the current num_turns. """
         for i in range(1, self.num_disks+1):
-            position = self.get_pos(i, turn_count)
+            position = self.get_pos(i, self.num_turns)
             if position == 0:
                 self.L.append(i)
             elif position == 1:
@@ -39,11 +32,11 @@ class TowersOfHanoi:
             self.M = ([0] * (self.num_disks - len(self.M))) + self.M
         if len(self.R) < self.num_disks:
             self.R = ([0] * (self.num_disks - len(self.R))) + self.R
-
         self.render_state()
-    def get_pos(self, disk_size, current_turn):
-      
-        ''' Returns the position of a disk in the game state give the disk size and the current turn.'''
+
+    def get_pos(self, disk_size: int, current_turn: int) -> int:
+        """ Returns the position of a disk in the game state give the disk size and the current turn. """
+        
         disk_direction = self.direction * [1,-1][disk_size % 2 == 0]
         moves = []
         start = 2 ** (disk_size - 1)
@@ -60,8 +53,8 @@ class TowersOfHanoi:
         return position
 
 
-    def build_disk(self, disk_size, isEnd):
-      ''' Returns a string that contains an ASCII generated image of the disk.'''
+    def build_disk(self, disk_size: int, isEnd: bool) -> str:
+        """ Returns a string that contains an ASCII generated image of the disk. """
       
         disk = "#" * disk_size
         if disk_size == 1:
@@ -71,9 +64,8 @@ class TowersOfHanoi:
             return pad + disk
         return pad + disk + pad + " "
 
-    def render_state(self):
-      ''' Prints the current state of the game, as well as expected number of turns to 
-          complete a new game with the given number of disks. '''
+    def render_state(self) -> None:
+        """ Prints the current state of the game, as well as expected number of turns to complete a new game with the given number of disks. """
       
         for i in range(self.num_disks):
             L_disk = self.build_disk((2 * self.L[i]) + 1, 0)
@@ -84,8 +76,21 @@ class TowersOfHanoi:
             else:
                 self.state += L_disk + M_disk + R_disk + '\n'
 
-        print(self.state)
-        remaining_moves = (2**self.num_disks) - 1
-        print(remaining_moves)
+    def __str__(self) -> str:
+        remaining_moves = '\n' + str((2**self.num_disks) - 1)
+        return self.state + remaining_moves
 
-game = TowersOfHanoi(disk_count, turn_count)
+def main():
+    try:
+        disk_count = int(input())
+        turn_count = int(input())
+
+    except ValueError:
+        print("Please enter valid integers.")
+        sys.exit(1)
+
+    game = TowersOfHanoi(disk_count, turn_count)
+    print(game)
+
+if __name__ == "__main__":
+    main()
