aa73a5bccd5aa2aa19aa3087494cce5ef1a6c21d
Joseph Garcia <63897711+Amheht@users.noreply.github.com>
2025-11-13 11:32:05 -0600

    Update comment for authorship clarity

--- Easy/rotating-arrows/solution.py
+++ Easy/rotating-arrows/solution.py
@@ -1,4 +1,5 @@
-# Written by Joseph Garcia
+# Solution written by Joseph Garcia
+# Original Puzzle: https://www.codingame.com/training/easy/rotating-arrows
 
 # Constants & Declarations
 ARROW_MAP = ["^", ">", "v", "<"]
