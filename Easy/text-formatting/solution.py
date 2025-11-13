7ff2bb04d1bac2da0b04b19527bfdea9c9bf99d3
Joseph Garcia <63897711+Amheht@users.noreply.github.com>
2025-11-13 11:32:47 -0600

    Clarify authorship in solution.py
    Updated the comment to clarify authorship and provide a link to the original puzzle.

--- Easy/text-formatting/solution.py
+++ Easy/text-formatting/solution.py
@@ -1,4 +1,5 @@
-# Written by Joseph Garcia
+# Solution written by Joseph Garcia
+# Original Puzzle: https://www.codingame.com/training/easy/text-formatting
 import re
 import string
 intext = input()
